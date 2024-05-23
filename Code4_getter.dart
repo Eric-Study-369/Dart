623f584fab4b84ac056f26872cd0e3af73683428
Eric-study-369 <jakeson270@gmail.com>
2024-05-23 14:58:11 +0700

    release of initiali

--- Code4_getter.dart
+++ Code4_getter.dart
@@ -38,6 +38,7 @@ void main(){
  */
 
 //-- code_3
+/*
 class NoteBook{
   String _name;
   double _prize;
@@ -62,4 +63,65 @@ void main(){
   //
   print("Note Book name ${nu2.name}");
   print("Not Book price ${nu2.price}");
+}
+
+// *
+class Notebook {
+  String _name;
+  double _prize;
+
+  Notebook (this._name  , this._prize);
+
+  String get Name {
+    if (_name == "") {
+      return "Np name " ;
+    }
+    return this._name;
+  }
+
+  double get price {
+    if (_prize == 200.0){
+      print("yess good");
+    }
+
+    return this._prize;
+  }
+
+}
+void main() {
+  Notebook nb =new Notebook ("srun" ,200.0 );
+  print("First Notebook ${nb.Name}");
+  print("First Notebook ${nb.price}");
+  
+  Notebook nb2 = new Notebook("", 300);
+  print("NoteBook Name : ${nb2._name}");
+  print("notBook Prce : ${nb2.price}");
+}
+
+ */
+
+
+ class Doctor {
+  String _name;
+
+  int _age;
+  String _gender;
+
+  Doctor(this._name, this._age, this._gender);
+
+  String get name => _name;
+
+  int get age => _age;
+
+  String get geder => _gender;
+
+  Map<String, dynamic> get map {
+    return {"Name ": _name ,"age": _age, "gender": _gender
+    };
+  }
+
+}
+void main(){
+  Doctor Do = Doctor("jake", 55, 'M');
+  print(Do.map);
 }
--- D2_Constructor.dart
+++ D2_Constructor.dart
@@ -36,24 +36,86 @@ print("shool Name : ${student.schoolname}");
 
 //Named constructor
 
-class Student{
+/*class Student{
   String? name;
   int? age;
   int? rollNumber;
 
-  Student(){
-    print("Defual constructor : ");
-
-  }
-  Student.namedconstructor(this.name, this.age ,)
+  Student (this.name , this.age, this.rollNumber);
+  Student.namedConstructo(this.name , this.age , [this.rollNumber = 0] );
 
 
-//void
+void display(){
+  print("Name : $name");
+  print("Age : $age");
+  print("RollNumber : $rollNumber ");
+}
+}
+
 
 void main(){
 
-  Student student = Student.namedconstructor("kake", 22, 4);
-  print("Name : ${student.name}");
-  print("Age : ${student.age}");
-  print("Rollnumber : ${student.rollNumber}");
+  var stu = Student("sky", 12, 200);
+  stu.display();
+  var stu2 = Student.namedConstructo("jAKE", 20);
+  stu2.display();
+  //Student student = Student("jake", 12, 02);
+  //Student.namedConstructo(, age)
+
+
 }
+
+ */
+
+import 'dart:convert';
+class Person{
+
+  String? name;
+  int? age;
+
+  Person(this.name , this.age);
+
+ /* Person.convert1(Map < String , dynamic> Json){
+    name = Json['name'];
+    age = Json['age'];
+  }
+
+  */
+
+
+
+  Person.convert2(String jsonString){
+    Map<String,dynamic> json = jsonDecode(jsonString);
+    name = json['name'];
+    age = json['age'];
+  }
+}
+
+void main() {
+  String fromGijo1 = '{"name" : "Bisho" , "age" : 25}';
+  String fromGijo2 = '{"name" : "John" , "age" : 34}';
+
+  Person p1 = Person.convert2(fromGijo1);
+  print("Person 1 name : ${p1.name}");
+ print("Peron 1 age : ${p1.age}");
+
+ Person p2 = Person.convert2(fromGijo2);
+ print("Person 2 name : ${p2.name}");
+ print("Person 2 age : ${p2.age}");
+}
+
+
+/*class Point {
+  final int x ;
+  final int y ;
+  
+  const Point(this.x , this.y);
+}
+
+void main(){
+  
+  Point point1 = Point(2, 7);
+  print("The p1 hash code is: ${point1.hashCode}";
+}
+
+ */
