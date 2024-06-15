16090bf5b789cf97be7087f5c8b8a9f2c5caa060
Eric-study-369 <jakeson270@gmail.com>
2024-06-15 14:24:03 +0700

    none(null_safety): test logic code

--- null_safaty/type_promotion.dart
+++ null_safaty/type_promotion.dart
@@ -1,5 +1,15 @@
-void main(){
+/*void main(){
   List <String> drinks = ["water" , " juice " "cake"];
   print("First element of the list is : ${drinks.first}");
   print("last element of the is : ${drinks.last}");
+}/
+ */
+
+void main(){
+  print("sky");
+  print("sky");
+  print("sky");
+  print("sky");
+  print("sky");
+  print("sky");
 }
