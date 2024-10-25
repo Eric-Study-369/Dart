5d83594cb7de9e1bba13920961ebc26dd135da45
Eric-study-369 <jakeson270@gmail.com>
2024-10-25 23:44:11 +0700

    add(new file dart_doucument/function)

---
+++ dart_tutorial/dart_doucument/function/function.dart
@@ -0,0 +1,45 @@
+
+void main(){
+
+  // optional positional parameters
+  String say(String from, String msg, [String? devices]){
+     var result = '$from says $msg';
+     if(devices != null){
+      result = '$result with a $devices';
+     }
+     return result;
+  }
+  // calling functiion without the optional paramater
+  assert(say('Bob', 'Howdy') == 'Bob say Howdy' ); 
+
+  // calling function with optional parameter 
+  // add 'smoke signal' for third argument
+  assert(say('Bob', 'Howdy', 'smoke signal') == 'Bob says Howdy with a smoke signal');
+  print(say('Bob', 'Howdy'));
+
+  // use = to specify a default value
+  String says(String from, String msg, [String device = 'carrier pigeon']) 
+  {
+   var result = '$from says $msg with a $device';
+   return result;
+ }
+  assert(says('Bob', 'Howdy') == 'Bob says Howdy with a carrier pigeon');
+  print(says('Bob', 'Howdy'));
+ 
+
+
+
+  // anonymous function
+  const list = ['apple', 'banana', 'orige'];
+
+  var uppercaseList = list.map((item){  // (item) { return item.toUpperCase(); } is an anonymous function
+                                        // .map() is apply function element to list
+
+    return item.toUpperCase();
+
+  }).toList(); // .toList() converts the result back into a List since .map() 
+
+  for (var item in uppercaseList){
+    print('$item: ${item.length}'); // print item followed by length
+  }
+}
